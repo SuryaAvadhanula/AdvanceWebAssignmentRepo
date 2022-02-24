004aabe5f218fecdefc5e04bf14f3fbdf5344cba
SuryaAvadhanula <suryatejasharma@gmail.com>
2022-02-24 17:02:37 -0500

    added components in vue

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Nav from './components/nav.vue';
+import Messages from './pages/Messages.vue'
+</script>
+
+<template>
+<Nav/>
+<h1 class="title">
+   Welcome to the Vue project
+ </h1>
+<Messages/>
+ 
+</template>
+
+<style scoped>
+
+</style>
