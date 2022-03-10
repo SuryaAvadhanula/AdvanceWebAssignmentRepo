ffecf92a3728e992a524cbd95f24faf3d6b909b2
SuryaAvadhanula <suryatejasharma@gmail.com>
2022-03-10 12:57:35 -0500

    updated & included tasks created by all users in the view in allTasks page

--- client/src/models/user.ts
+++ client/src/models/user.ts
@@ -19,9 +19,9 @@ export const list: User[] = [
         email: 'l@gmail.com',
         picture: 'https://randomuser.me/api/portraits/lego/4.jpg',
         userTasks: [
-            {task : 'Need to update vue',dueDate:'04/21/2022', isCompleted: true, assignedBy: "Kira"},
-            {task : 'Need to update pinia',dueDate:'05/18/2022', isCompleted: false, assignedBy: "OnePiece"},
-            {task : 'Need to update express',dueDate:'03/11/2022', isCompleted: true, assignedBy: "Kira"}
+            {task : 'Need to update vue',dueDate:'2022-05-16', isCompleted: true, assignedBy: "Kira"},
+            {task : 'Need to update pinia',dueDate:'2022-01-11', isCompleted: false, assignedBy: "OnePiece"},
+            {task : 'Need to update express',dueDate:'2022-02-28', isCompleted: true, assignedBy: "Kira"}
         ]
     },
     {
@@ -33,8 +33,8 @@ export const list: User[] = [
         email: 'lyagami@gmail.com',
         picture: 'https://randomuser.me/api/portraits/lego/5.jpg',
         userTasks: [
-            {task : 'Need to update angular',dueDate:'03/21/2022', isCompleted: false, assignedBy: "Ryuk"},
-            {task : 'Need to update react',dueDate:'03/26/2022', isCompleted: false, assignedBy: "OnePiece"}
+            {task : 'Need to update angular',dueDate:'2022-03-21', isCompleted: false, assignedBy: "Ryuk"},
+            {task : 'Need to update react',dueDate:'2022-03-22', isCompleted: false, assignedBy: "OnePiece"}
         ]
     },
     {
@@ -46,8 +46,8 @@ export const list: User[] = [
         email: 'l@gmail.com',
         picture: 'https://randomuser.me/api/portraits/lego/2.jpg',
         userTasks: [
-            {task : 'Need to update jQuery',dueDate:'04/05/2022', isCompleted: false, assignedBy: "Kira"},
-            {task : 'Need to update ajax',dueDate:'03/25/2022', isCompleted: false, assignedBy: "Ryuk"}
+            {task : 'Need to update jQuery',dueDate:'2022-05-02', isCompleted: false, assignedBy: "Kira"},
+            {task : 'Need to update ajax',dueDate:'2022-07-02', isCompleted: false, assignedBy: "Ryuk"}
         ]
     }
 ]
