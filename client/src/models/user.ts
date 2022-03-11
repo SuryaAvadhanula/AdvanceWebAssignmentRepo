f561ffa5652bb7afbc42cd73cc5019c53b4634a9
SuryaAvadhanula <suryatejasharma@gmail.com>
2022-03-10 23:07:06 -0500

    updated user.ts

--- client/src/models/user.ts
+++ client/src/models/user.ts
@@ -20,7 +20,7 @@ export const list: User[] = [
         picture: 'https://randomuser.me/api/portraits/lego/4.jpg',
         userTasks: [
             {task : 'Need to update vue',dueDate:'2022-05-16', isCompleted: true, assignedBy: "Kira"},
-            {task : 'Need to update pinia',dueDate:'2022-01-11', isCompleted: false, assignedBy: "OnePiece"},
+            {task : 'Need to update pinia',dueDate:'2022-01-11', isCompleted: false, assignedBy: "Luffy"},
             {task : 'Need to update express',dueDate:'2022-02-28', isCompleted: true, assignedBy: "Kira"}
         ]
     },
@@ -33,8 +33,8 @@ export const list: User[] = [
         email: 'lyagami@gmail.com',
         picture: 'https://randomuser.me/api/portraits/lego/5.jpg',
         userTasks: [
-            {task : 'Need to update angular',dueDate:'2022-03-21', isCompleted: false, assignedBy: "Ryuk"},
-            {task : 'Need to update react',dueDate:'2022-03-22', isCompleted: false, assignedBy: "OnePiece"}
+            {task : 'Need to update angular',dueDate:'2022-03-21', isCompleted: false, assignedBy: "Lawliet"},
+            {task : 'Need to update react',dueDate:'2022-03-22', isCompleted: false, assignedBy: "Luffy"}
         ]
     },
     {
@@ -47,7 +47,7 @@ export const list: User[] = [
         picture: 'https://randomuser.me/api/portraits/lego/2.jpg',
         userTasks: [
             {task : 'Need to update jQuery',dueDate:'2022-05-02', isCompleted: false, assignedBy: "Kira"},
-            {task : 'Need to update ajax',dueDate:'2022-07-02', isCompleted: false, assignedBy: "Ryuk"}
+            {task : 'Need to update ajax',dueDate:'2022-07-02', isCompleted: false, assignedBy: "Lawliet"}
         ]
     }
 ]
